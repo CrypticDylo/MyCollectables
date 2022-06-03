a640c1a0a65ac1ac4e8ff961833ce2779c95a704
dustyDIAMOND45 <101386133+dustyDIAMOND45@users.noreply.github.com>
2022-06-03 14:26:27 +0200

    Login

--- app/src/main/java/com/varsitycollege/mycollectables/LoginScreen.java
+++ app/src/main/java/com/varsitycollege/mycollectables/LoginScreen.java
@@ -1,15 +1,31 @@
 package com.varsitycollege.mycollectables;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginScreen extends AppCompatActivity {
 
     private Button loginBtn;
+    Button sig;
+    EditText Email, passW;
+    ProgressBar proB;
+    FirebaseAuth fAuth;
+
 
 
     @Override
@@ -24,11 +40,90 @@ public class LoginScreen extends AppCompatActivity {
                 login();
             }
         });
+
+        sig = (Button)findViewById(R.id.CreateAcc);
+        sig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Reg();
+            }
+        });
+
+
+
+        Email = findViewById(R.id.usernameUserInput);
+        passW = findViewById(R.id.passwordUserInput);
+        proB = findViewById(R.id.progressBarLog);
+        fAuth = FirebaseAuth.getInstance();
+
+
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail = Email.getText().toString().trim();
+                String pass = passW.getText().toString().trim();
+
+                if (TextUtils.isEmpty(mail)){
+                    Email.setError("Please Enter your Email");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(pass)){
+                    passW.setError("Password is required");
+                    return;
+                }
+
+                if (pass.length() < 6){
+                    passW.setError("Password must be than 6 characters");
+                    return;
+                }
+
+                proB.setVisibility(View.VISIBLE);
+
+                //check if user has registered
+
+                fAuth.signInWithEmailAndPassword(mail,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                       if(task.isSuccessful()) {
+                           Toast.makeText(LoginScreen.this, "You have successfully logged in", Toast.LENGTH_SHORT).show();
+                           startActivity(new Intent(getApplicationContext(),CategoriesScreen.class));
+                       }else{
+                           Toast.makeText(LoginScreen.this, "An error has occurred  " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                           proB.setVisibility(View.GONE);
+                       }
+                    }
+                });
+            }
+
+
+        });
+
+
+
+
+
     }
+
+
+
+
 
     public void login(){
 
         Intent intent = new Intent(this, CategoriesScreen.class);
         startActivity(intent);
+    }
+
+    public void Reg(){
+
+        Intent intent = new Intent(this, signupScreen.class);
+        startActivity(intent);
+    }
+
+    public void sig(View view) {
     }
 }
--- app/src/main/java/com/varsitycollege/mycollectables/signupScreen.java
+++ app/src/main/java/com/varsitycollege/mycollectables/signupScreen.java
@@ -1,32 +1,117 @@
 package com.varsitycollege.mycollectables;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signupScreen extends AppCompatActivity {
 
-    private Button signupBtn;
+    //declare variables to work with objects in the activity_signup_screen
+
+    Button signupBtn;
+    EditText FName, UserName, Password;
+    Button Google, Facebook;
+    FirebaseAuth fAuth;
+    ProgressBar progressbar;
+    TextView Login;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
 
+        //saves the user entered values to the variables
+
+        FName = findViewById(R.id.usernameUserInput);
+        UserName = findViewById(R.id.nameUserInput);
+        Password = findViewById((R.id.passwordUserInput));
+        Google =findViewById(R.id.signupWGBtnSignupPage);
+        Facebook = findViewById(R.id.SignupWFBtnSignupPage);
         signupBtn = (Button) findViewById(R.id.signupBtnSignupPage);
+        Login = findViewById(R.id.alreadyHaveAccountText);
+
+        // Creates an Instance in firebase to capture the values
+
+        fAuth = FirebaseAuth.getInstance();
+        progressbar = findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),LoginScreen.class));
+            finish();
+        }
+
+        //sign up conditions to check if the values entered are valid
+
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signup();
+                String fullName =  FName.getText().toString().trim();
+                String UName = UserName.getText().toString().trim();
+                String passW = Password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(fullName)){
+                    FName.setError("Full Name is required");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(UName)){
+                    UserName.setError("Please Enter an Email");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(passW)){
+                    Password.setError("Password is required");
+                    return;
+                }
+
+                if (passW.length() < 6){
+                    Password.setError("Password must be than 6 characters");
+                    return;
+                }
+
+                progressbar.setVisibility(View.VISIBLE);
+
+                //register user in firebase
+
+                fAuth.createUserWithEmailAndPassword(UName,passW).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(signupScreen.this, "You have successfully signed up", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),LoginScreen.class));
+                        }else{
+                            Toast.makeText(signupScreen.this, "An error has occurred  " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressbar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),LoginScreen.class));
             }
         });
     }
 
-    public void signup(){
-        Intent intent = new Intent(this, LoginScreen.class);
-        startActivity(intent);
-    }
+
 }
