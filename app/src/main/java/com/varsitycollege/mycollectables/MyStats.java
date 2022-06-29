85ef64706efa5d46f1462f69a3ed167ae938fe65
DazzlaC <86716792+DazzlaC@users.noreply.github.com>
2022-06-29 17:12:31 +0200

    working graph ascending values

--- app/src/main/java/com/varsitycollege/mycollectables/MyStats.java
+++ app/src/main/java/com/varsitycollege/mycollectables/MyStats.java
@@ -1,6 +1,7 @@
 package com.varsitycollege.mycollectables;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,15 +11,46 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+
 public class MyStats extends AppCompatActivity {
+
+    BarChart barChart;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_stats);
+
+        //Assign variable
+        barChart = findViewById(R.id.bar_chart);
+
+        //Initialize array list
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+
+        //For loop
+        for(int i=1; i<10; i++){
+            float value = (float) (i*10.0);
+            BarEntry barEntry = new BarEntry(i,value);
+            barEntries.add(barEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries,"Collections");
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setDrawValues(false);
+        barChart.setData(new BarData(barDataSet));
+        barChart.animateY(5000);
+        barChart.getDescription().setText("Collection Chart");
+        barChart.getDescription().setTextColor(Color.BLUE);
+
 
     }
 
